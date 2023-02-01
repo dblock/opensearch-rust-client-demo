33a782668c5d82766f8aaaefb3baefa6db94e919
dblock <dblock@amazon.com>
2023-02-01 14:09:36 -0500

    Added support for OpenSearch Serverless.
    Signed-off-by: dblock <dblock@amazon.com>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # OpenSearch Ruby Client Demo
 
-Makes requests to Amazon OpenSearch using the [OpenSearch Rust Client](https://github.com/opensearch-project/opensearch-rs).
+Makes requests to [Amazon OpenSearch Service](https://aws.amazon.com/opensearch-service/) using the [OpenSearch Rust Client](https://github.com/opensearch-project/opensearch-rs). Supports [OpenSearch Serverless](https://aws.amazon.com/opensearch-service/features/serverless/) since version 2.1.0.
 
 ## Prerequisites
 
@@ -21,10 +21,10 @@ Create an OpenSearch domain in (AWS) which support IAM based AuthN/AuthZ.
 export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
 export AWS_SESSION_TOKEN=
-export AWS_REGION=us-west2
+export AWS_REGION=us-west-2
 
 export ENDPOINT=https://....us-west-2.es.amazonaws.com
-
+export SERVICE=es # use "aoss" for OpenSearch Serverless 
 cargo run
 ```
 
@@ -32,10 +32,39 @@ This will output the version of OpenSearch and a search result.
 
 ```
 opensearch: 2.3.0
-Object {"director": String("Bennett Miller"), "id": Number(1), "title": String("Moneyball"), "year": Number(2011)}
+
+{
+  "_shards": {
+    "failed": 0,
+    "skipped": 0,
+    "successful": 5,
+    "total": 5
+  },
+  "hits": {
+    "hits": [
+      {
+        "_id": "8qhYDoYBpJvbGlZLDVk_",
+        "_index": "movies",
+        "_score": 0.2876821,
+        "_source": {
+          "director": "Bennett Miller",
+          "title": "Moneyball",
+          "year": 2011
+        }
+      }
+    ],
+    "max_score": 0.2876821,
+    "total": {
+      "relation": "eq",
+      "value": 1
+    }
+  },
+  "timed_out": false,
+  "took": 4
+}
 ```
 
-The [code](src/main.rs) will create an index, add a document, then cleanup.
+The [code](src/main.rs) creates an index, adds a document, then cleans up.
 
 ## License 
 
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use aws_config::meta::region::RegionProviderChain;
     use std::{convert::TryInto, env, thread, time};
 
     use serde_json::Value;
@@ -22,20 +23,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     use url::Url;
 
+    tracing_subscriber::fmt::init();
+
     let url = Url::parse(&env::var("ENDPOINT").expect("Missing ENDPOINT"));
+    let service_name = &env::var("SERVICE").unwrap_or("es".to_string());
     let conn_pool = SingleNodeConnectionPool::new(url?);
-    let aws_config = aws_config::load_from_env().await.clone();
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    let aws_config = aws_config::from_env().region(region_provider).load().await.clone();
     let transport = TransportBuilder::new(conn_pool)
         .auth(aws_config.clone().try_into()?)
+        .service_name(service_name)
         .build()?;
     let client = OpenSearch::new(transport);
 
-    let info: Value = client.info().send().await?.json().await?;
-    println!(
-        "{}: {}",
-        info["version"]["distribution"].as_str().unwrap(),
-        info["version"]["number"].as_str().unwrap()
-    );
+    // TODO: remove when OpenSearch Serverless adds support for GET /
+    if service_name == "es" {
+        let info: Value = client.info().send().await?.json().await?;
+        println!(
+            "{}: {}",
+            info["version"]["distribution"].as_str().unwrap(),
+            info["version"]["number"].as_str().unwrap()
+        );
+    }
 
     let index_name = "movies";
 
@@ -46,9 +55,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     client
-        .index(opensearch::IndexParts::IndexId(index_name, "1"))
+        .index(opensearch::IndexParts::Index(index_name))
         .body(serde_json::json!({
-                "id": 1,
                 "title": "Moneyball",
                 "director": "Bennett Miller",
                 "year": 2011
@@ -57,7 +65,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .send()
         .await?;
 
-    thread::sleep(time::Duration::from_secs(1));
+    thread::sleep(time::Duration::from_secs(5));
 
     let response = client
         .search(opensearch::SearchParts::Index(&[index_name]))
@@ -73,9 +81,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     let response_body = response.json::<Value>().await?;
-    for hit in response_body["hits"]["hits"].as_array().unwrap() {
-        println!("{:?}", hit["_source"]);
-    }
+    println!("{}", serde_json::to_string_pretty(&response_body).unwrap());
 
     client
         .indices()
